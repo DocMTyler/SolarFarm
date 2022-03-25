4342db959740ff078772041aecd83a6222016c1d
Tyler Hougland <mtylerhouglandphd@gmail.com>
2022-03-25 12:14:35 -0500

    Initial commit

---
+++ SolarFarm/SolarFarm.Core/Material.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarFarm.Core
+{
+    public enum Material
+    {
+        MULTICRYSTALLINESILICON = 1,
+        MONOCRYSTALLINESILICON,
+        AMORPHOUSSILICON,
+        CADMIUMTELLURIDE,
+        COPPERINDIUMGALLIUMSELENIDE
+    }
+}
---
+++ SolarFarm/SolarFarm.Core/Panel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SolarFarm.Core
+{
+    public class Panel
+    {
+        public string Section { get; set; }
+
+        public int Row { get; set; }
+        
+        public int Column { get; set; }
+        
+        public DateTime Year { get; set; }
+
+        public bool IsTracking { get; set; }
+
+        public Material Material { get; set; }
+    }
+}
