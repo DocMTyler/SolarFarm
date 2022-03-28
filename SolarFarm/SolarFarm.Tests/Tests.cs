bc1c0423b1f93b609c988492c5101c18e0c06fbc
Tyler Hougland <mtylerhouglandphd@gmail.com>
2022-03-28 09:11:40 -0500

    Add repo testing

--- SolarFarm/SolarFarm.Tests/Tests.cs
+++ SolarFarm/SolarFarm.Tests/Tests.cs
@@ -13,7 +13,7 @@ namespace SolarFarm.Tests
         PanelService service = new(repo);
 
         [Test]
-        public void AddReturnsTrue()
+        public void AddServiceReturnsTrue()
         {
             Panel panel = new();
             panel.Section = "North";
@@ -27,7 +27,7 @@ namespace SolarFarm.Tests
         }
 
         [Test]
-        public void RemoveReturnsFalse()
+        public void RemoveServiceReturnsFalse()
         {
             Panel panel = new();
             panel.Section = "North";
@@ -41,7 +41,57 @@ namespace SolarFarm.Tests
         }
 
         [Test]
-        public void UpdateReturnsTrue()
+        public void UpdateServiceReturnsTrue()
+        {
+            Panel panel1 = new();
+            panel1.Section = "North";
+            panel1.Row = 1;
+            panel1.Column = 1;
+            panel1.YearInstalled = DateTime.Parse("1/1/2020");
+            panel1.Material = (Material)4;
+            panel1.IsTracking = true;
+
+            Panel panel2 = new();
+            panel2.Section = "North";
+            panel2.Row = 1;
+            panel2.Column = 1;
+            panel2.YearInstalled = DateTime.Parse("1/1/2020");
+            panel2.Material = (Material)2;
+            panel2.IsTracking = true;
+
+            Assert.IsTrue(service.Update(panel1, panel2));
+        }
+
+        [Test]
+        public void AddRepoReturnsTrue()
+        {
+            Panel panel = new();
+            panel.Section = "North";
+            panel.Row = 2;
+            panel.Column = 1;
+            panel.YearInstalled = DateTime.Parse("1/1/2020");
+            panel.Material = (Material)4;
+            panel.IsTracking = true;
+
+            Assert.IsTrue(service.Add(panel));
+        }
+
+        [Test]
+        public void RemoveRepoReturnsFalse()
+        {
+            Panel panel = new();
+            panel.Section = "North";
+            panel.Row = 3;
+            panel.Column = 1;
+            panel.YearInstalled = DateTime.Parse("1/1/2020");
+            panel.Material = (Material)4;
+            panel.IsTracking = true;
+
+            Assert.IsFalse(service.Remove(panel));
+        }
+
+        [Test]
+        public void UpdateRepoReturnsTrue()
         {
             Panel panel1 = new();
             panel1.Section = "North";
